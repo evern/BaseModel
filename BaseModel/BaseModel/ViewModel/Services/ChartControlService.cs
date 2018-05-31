582daa4ab896ff26a99a891ffe8c2c806f590932
Evern, Su <evern@outlook.com>
2018-05-31 14:59:59 +0800

    - Added chart control service

---
+++ BaseModel/BaseModel/ViewModel/Services/ChartControlService.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Mvvm.UI;
+using DevExpress.Xpf.Charts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BaseModel.ViewModel.Services
+{
+    public interface IChartControlService
+    {
+        void Animate();
+    }
+
+    public class ChartControlService : ServiceBase, IChartControlService
+    {
+        public ChartControl ChartControl
+        {
+            get { return (ChartControl)GetValue(ChartControlProperty); }
+            set { SetValue(ChartControlProperty, value); }
+        }
+
+        public static readonly DependencyProperty ChartControlProperty = DependencyProperty.Register("ChartControl", typeof(ChartControl), typeof(ChartControlService), new PropertyMetadata(null));
+
+        public void Animate()
+        {
+            ChartControl.Animate();
+        }
+    }
+}
