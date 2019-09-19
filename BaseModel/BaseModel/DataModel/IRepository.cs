31cf34e03b240b41a128f25a2b02a76678fbcea8
Evern, Su <evern@outlook.com>
2019-09-19 17:34:51 +0800

    - add a new clause to cast projection as TEntity without having to find repository by primary key, will speed up process for simple views tremendously but deleted entities aren't verified before saving

--- BaseModel/BaseModel/DataModel/IRepository.cs
+++ BaseModel/BaseModel/DataModel/IRepository.cs
@@ -252,10 +252,18 @@ namespace BaseModel.DataModel
             TEntity entity = null;
             if (!isGuidEmpty)
             {
-                entity = repository.Find(projectionPrimaryKey);
-                //entity properties might be different from projection when projection is from another view model
-                if (entity != null && typeof(TEntity) == typeof(TProjection))
-                    DataUtils.ShallowCopy(entity, projectionEntity);
+                if (!projection)
+                {
+                    //speed up process, draw back is deleted entity is not checked
+                    entity = projectionEntity as TEntity;
+                    //entity properties might be different from projection when projection is from another view model
+                    if (entity != null)
+                        DataUtils.ShallowCopy(entity, projectionEntity);
+                }
+                else
+                {
+                    entity = repository.Find(projectionPrimaryKey);
+                }
             }
 
             if (entity == null || isGuidEmpty)
