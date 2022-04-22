8e986894bf640e6d536d946907ed5e62372d2dab
evern <evern@outlook.com>
2022-04-22 16:42:43 +0800

    - added BulkEditTime view

---
+++ BaseModel/BaseModel/View/BulkEditTime.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace BaseModel.View
+{
+    /// <summary>
+    /// Interaction logic for BulkEditStrings.xaml
+    /// </summary>
+    public partial class BulkEditTime : UserControl
+    {
+        public BulkEditTime()
+        {
+            InitializeComponent();
+        }
+    }
+}
